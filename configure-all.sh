633d6b6e282a5be1ca67d009a34f21db9c906b60
Ángel Careaga <ing.angelcareaga@gmail.com>
2019-01-28 14:03:59 -0600

    OTI | Uncomment lines to run

--- configure-all.sh
+++ configure-all.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 ## Load configurations
+echo " ======= LOADING CONFIGURATIONS ======"
 . "utils/parse_yaml.sh"
 eval $(parse_yaml config-ocp-tools.yml "cfg_")
 
-echo $cfg_username_subscription
-
-#
+echo " ======= CONFIGURE ========"
+#Evaluate if use Satellite or direct
 if [ $cfg_satellite_subs = "true" ]; then
-    #sh satellite-provider/configure_bastion_satellite.sh
+    sh satellite-provider/configure_bastion_satellite.sh
     else
-    #sh subscription-provider/configure_bastion_subscription.sh
+    sh subscription-provider/configure_bastion_subscription.sh
 fi
 
